e034d0b5d4504363a590ca805b84d3a034f09b0c
Sanlex <thesanlex93@gmail.com>
2026-07-13 18:46:02 +0000

    First commit after creating a repo

---
+++ backend/app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+import os
+import psycopg2
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Hello from Backend!"
+
+
+@app.route("/db")
+def db():
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST", "db"),
+            database=os.getenv("DB_NAME", "appdb"),
+            user=os.getenv("DB_USER", "admin"),
+            password=os.getenv("DB_PASSWORD", "admin"),
+            port=os.getenv("DB_PORT", "5432"),
+        )
+
+        cur = conn.cursor()
+        cur.execute("SELECT version();")
+        version = cur.fetchone()[0]
+
+        cur.close()
+        conn.close()
+
+        return f"Connected to PostgreSQL!<br><br>{version}"
+
+    except Exception as e:
+        return f"Database connection failed:<br><br>{e}", 500
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
